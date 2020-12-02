1ae2d3353a5ef589244169f4886de60c66533c9c
atmlhv <nandeglaywww@gmail.com>
2020-12-03 01:40:27 +0900

    アルパカの首が伸びてつばをはけるようにしました
    space押しで首がのびる
    enter長押しでつばが早くなって重力が減る

---
+++ Assets/alpacaManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alpacaManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject kubiprefab = default;
+    [SerializeField]
+    GameObject kubinearhead = default;
+    [SerializeField]
+    GameObject atama = default;
+    [SerializeField]
+    GameObject tamaprefab = default;
+
+    Vector3 defaultpos_atama;
+    Vector3 defaultpos_kubi;
+
+    const float kubiupspeed = 0.01f;
+    const float kubidownspeed = 0.01f;
+
+    int addkubinum = 0;
+    float kubisizey;
+    List<GameObject> kubis = new List<GameObject>();
+
+    float returnkeyframes = 0f;
+    const float returnkeythreshold = 15f;
+
+    private void Start()
+    {
+        defaultpos_atama = atama.transform.position;
+        defaultpos_kubi = kubinearhead.transform.position;
+
+        kubis.Add(kubinearhead);
+
+        kubisizey = kubiprefab.GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) //首伸ばす
+        {
+            foreach(var kubi in kubis)
+            {
+                kubi.transform.position += new Vector3(0, kubiupspeed);
+            }
+            atama.transform.position += new Vector3(0, kubiupspeed);
+        }
+        else //首縮める
+        {
+            if (defaultpos_atama.y < atama.transform.position.y)
+            {
+                foreach (var kubi in kubis)
+                {
+                    kubi.transform.position -= new Vector3(0, kubidownspeed);
+                }
+                atama.transform.position -= new Vector3(0, kubidownspeed);
+            }
+        }
+
+        //首減らす
+        if (kubinearhead.transform.position.y - defaultpos_kubi.y <= kubisizey / 2 * (addkubinum - 1))
+        {
+            addkubinum--;
+            Destroy(kubis[kubis.Count - 1]);
+            kubis.RemoveAt(kubis.Count - 1);
+        }
+
+        //首増やす
+        if (kubinearhead.transform.position.y - defaultpos_kubi.y > kubisizey / 2 * addkubinum)
+        {
+            addkubinum++;
+            kubis.Add(Instantiate(kubiprefab, kubinearhead.transform.position - new Vector3(0,kubisizey/2*addkubinum), Quaternion.identity));
+        }
+
+        //弾発射
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            GameObject obj = Instantiate(tamaprefab, atama.transform.position, Quaternion.identity);
+            obj.GetComponent<spitController>().spit_initialize(returnkeyframes);
+            returnkeyframes = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //弾発射用のフレーム数計測
+        if (Input.GetKey(KeyCode.Return))
+        {
+            returnkeyframes++;
+        }
+    }
+}
---
+++ Assets/spitController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spitController : MonoBehaviour
+{
+    const float spitspeed = 0.5f;
+    float gravity = 0.05f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity -= new Vector2(0, gravity);
+    }
+
+    public void spit_initialize(float frame)
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(spitspeed * frame, 0);
+        gravity /= frame;
+    }
+}
