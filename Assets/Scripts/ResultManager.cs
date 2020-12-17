910da34a1b94fe93fb478cde64036b2377d969ab
kaquas <75485422+kaquas@users.noreply.github.com>
2020-12-18 02:40:38 +0900

    1.0.6
    スコア、開始前、終了後演出追加

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//https://yanpen.net/unity/countup_score_animation/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -16,7 +18,8 @@ public class ResultManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Score.text = string.Format("Score:{0}", ScoreManager.score);
+        ScoreManager.score = 1300;
+
         if (ScoreManager.score >= 1000 && ScoreManager.score<2000)
         {
             ResultImage2.SetActive(true);
@@ -30,7 +33,39 @@ public class ResultManager : MonoBehaviour
             ResultImage1.SetActive(true);
         }
         fade.FadeOut(0.5f);
+        //3秒かけてスコアを加算していく。
+        StartCoroutine(ScoreAnimation(0f,(float)ScoreManager.score, 3f));
+
     }
+
+    private IEnumerator ScoreAnimation(float startScore, float endScore, float duration)
+    {
+        // 開始時間
+        float startTime = Time.time;
+
+        // 終了時間
+        float endTime = startTime + duration;
+
+        do
+        {
+            // 現在の時間の割合
+            float timeRate = (Time.time - startTime) / duration;
+
+            // 数値を更新
+            float updateValue = (float)((endScore - startScore) * timeRate + startScore);
+
+            // テキストの更新
+            Score.text = string.Format("Score:{0}", (int)updateValue);
+
+            // 1フレーム待つ
+            yield return null;
+
+        } while (Time.time < endTime);
+
+        // 最終的な着地のスコア
+        Score.text = string.Format("Score:{0}", (int)endScore);
+    }
+
 
     // Update is called once per frame
     void Update()
