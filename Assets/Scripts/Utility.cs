f014fc07c0f049e375288855152bdaf5c44e6253
atmlhv <nandeglaywww@gmail.com>
2020-12-20 04:42:18 +0900

    敵まわり色々実装
    敵のアニメーション、敵の出現位置を正規分布っぽく、地面の敵を実装

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Utility : MonoBehaviour
 {
     static Camera _mainCamera;
+    //static StreamWriter sw = new StreamWriter("../TextData.txt", false);// TextData.txtというファイルを新規で用意
 
     private void Awake()
     {
@@ -34,5 +36,27 @@ public class Utility : MonoBehaviour
     public static Vector3 getScreenPoint(Vector3 pos)
     {
         return _mainCamera.ScreenToWorldPoint(pos);
+    }
+
+    public static float getGaussianDistribution(float min, float max)
+    {
+        double sigma = (max - min) / 4f; //ここの除数を適宜変えれば分布も変わる
+        double ave = (max + min) / 2f;
+        double rnd1,rnd2;
+        double result = Mathf.Infinity;
+
+        while (result < min || result > max)
+        {
+            //rnd1が0だと死ぬ
+            while ((rnd1 = UnityEngine.Random.Range(0f, 1f)) == 0.0) ;
+            rnd2 = UnityEngine.Random.Range(0f, 1f);
+
+            result = sigma * System.Math.Sqrt(-2.0 * System.Math.Log(rnd1)) * System.Math.Cos(2.0 * System.Math.PI * rnd2) + ave;
+        }
+
+        //string txt = result.ToString();
+        //sw.WriteLine(txt);// ファイルに書き出したあと改行
+
+        return (float)result;
     }
 }
--- Assets/Scripts/enemyManager.cs
+++ Assets/Scripts/enemyManager.cs
@@ -6,6 +6,8 @@ public class enemyManager : MonoBehaviour
 {
     [SerializeField]
     GameObject tekiprefab = default;
+    [SerializeField]
+    GameObject tekilandprefab = default;
 
     public static int nowtekinum;
     int tekinum;
@@ -18,6 +20,9 @@ public class enemyManager : MonoBehaviour
 
     const int tekiframethresholdmin = 10;
     const int tekiframethresholdmax = 40;
+
+    const float tekilandrate = 0.1f;
+    const float tekilandposy = 0.5f;
 
     Vector3 bottomright;
     float screenheight;
@@ -37,15 +42,30 @@ public class enemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(nowtekinum < tekinum && tekiframecount > tekiframethreshold)
+
+        if (nowtekinum < tekinum && tekiframecount > tekiframethreshold)
         {
             int t = 60 - (int)TimeManager.seconds;
             tekiframethreshold = (int)(coefficient(tekiframethresholdmax, tekiframethresholdmin) * t * t) + tekiframethresholdmax;
             tekinum = (int)(coefficient(tekinummax, tekinummin) * t * t) + tekinummax;
             nowtekinum++;
             tekiframecount = 0;
-            GameObject teki = Instantiate(tekiprefab, bottomright + new Vector3(0f, UnityEngine.Random.Range(0, screenheight)), Quaternion.identity);
-            teki.GetComponent<tekiController>().tekiinitialize();
+
+            //敵の出る場所処理
+            if (UnityEngine.Random.value >= tekilandrate)
+            {
+                //飛ぶ敵
+                float tekiposy = Utility.getGaussianDistribution(screenheight / 8f, screenheight);
+                GameObject teki = Instantiate(tekiprefab, bottomright + new Vector3(0f, tekiposy), Quaternion.identity);
+                teki.GetComponent<tekiController>().tekiinitialize();
+            }
+            else
+            {
+                //地上
+                float tekiposy = tekilandposy;
+                GameObject teki = Instantiate(tekilandprefab, bottomright + new Vector3(0f, tekiposy), Quaternion.identity);
+                teki.GetComponent<tekiController>().tekiinitialize();
+            }
         }
     }
 
